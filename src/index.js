3f4766c624318665bf902feb34b12a17ca974a37
SvetlanaForte-2000 <105925303+SvetlanaForte-2000@users.noreply.github.com>
2025-08-19 21:31:35 +0300

    fix: implement GuessingGame binary search logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ class GuessingGame {
   constructor() {
     this.min = 0;
     this.max = 0;
-    this.current = 0;
+    this.currentGuess = 0;
   }
 
   setRange(min, max) {
@@ -11,16 +11,16 @@ class GuessingGame {
   }
 
   guess() {
-    this.current = Math.floor((this.min + this.max) / 2);
-    return this.current;
+    this.currentGuess = Math.round((this.min + this.max) / 2);
+    return this.currentGuess;
   }
 
   lower() {
-    this.max = this.current - 1;
+    this.max = this.currentGuess;
   }
 
   greater() {
-    this.min = this.current + 1;
+    this.min = this.currentGuess;
   }
 }
 
